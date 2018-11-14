92684d3c49e74786e409074869b8186569736b99
Rekha <pvrekha2018@gmail.com>
2018-11-14 12:54:41 -0500

    Day 22 assignment

---
+++ WhiteAssignments/src/day22/Printlinks.java
@@ -0,0 +1,37 @@
+package day22;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Printlinks {
+	
+WebDriver driver;
+
+	@BeforeTest()
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+		
+	@Test
+	public void findLinks() {
+		List<WebElement> link = driver.findElements(By.cssSelector("#js_0 > ul"));
+		//LinkedList<String>links = new LinkedList<>();
+		//for (Object  x: link ) {
+		//	System.out.println(x);
+		for(int i=0; i<link.size();i++) {
+			System.out.println("Printing links  " +link.get(i).getText());
+		}
+				}
+		}
+	
+
---
+++ WhiteAssignments/src/day22/TimeandDate.java
@@ -0,0 +1,65 @@
+package day22;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TimeandDate {
+
+
+WebDriver driver;
+	
+	public static boolean dateValidity(String date) {
+		boolean validation = true;
+		try {
+			DateTimeFormatter formatter  = DateTimeFormat.forPattern("MMM dd, yyyy");
+			DateTime currentTime = formatter.parseDateTime(date);
+		}
+		catch(Exception e) {
+			validation = false;
+		}
+		return validation;
+	}
+
+	@BeforeTest
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.timeanddate.com/");
+	}
+	
+	@Test
+	public void dateVerification() throws InterruptedException {
+		//String date = driver.findElement(By.id("ij2")).getText();
+		String date = driver.findElement(By.cssSelector("#ij2")).getText();
+		Thread.sleep(2000);
+		//String date = driver.findElement(By.xpath(("//span[contains(@id='ij2']"))));
+		System.out.println(date);
+		Assert.assertTrue(dateValidity(date));		
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
