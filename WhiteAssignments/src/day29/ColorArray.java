f2b3b65a4dc1384afae74708eccf4d1da09f7ed7
Rekha <pvrekha2018@gmail.com>
2018-11-27 13:10:04 -0500

    Assignment 1 from Day29 ,Assignment 3:Answer is "B"

---
+++ WhiteAssignments/src/day29/ColorArray.java
@@ -0,0 +1,26 @@
+package day29;
+
+import java.util.ArrayList;
+
+public class ColorArray {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> colors = new ArrayList<>();
+		colors.add("White");
+		colors.add("Orange");
+		colors.add("Yellow");
+		colors.add("Red");
+		colors.add("Green");
+		colors.add("New York");
+
+		for (String t : colors)
+			if (t.equals("Red")||(t.equals("Orange"))){
+				System.out.println("Have dinner in a restaurant");
+			}
+			 else {
+				System.out.println("Today is Friday");
+			}
+
+	}
+}
