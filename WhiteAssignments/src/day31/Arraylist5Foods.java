39bdc745db5e0a7c6f25952f4b20fe601dd1997a
Rekha <pvrekha2018@gmail.com>
2018-11-29 13:33:16 -0500

    Assignments 1 & # from day31. 2 will follow

---
+++ WhiteAssignments/src/day31/Arraylist5Foods.java
@@ -0,0 +1,25 @@
+package day31;
+
+import java.util.ArrayList;
+
+public class Arraylist5Foods {
+	
+	public static void main(String[] args) {
+		ArrayList<String>junk = new ArrayList<>();
+		
+		junk.add("Dosa");
+		junk.add("Idli");
+		junk.add("Roti");
+		junk.add("Vada");
+		junk.add("Pakoda");
+	
+	
+	//for (String i:junk) {
+    	//System.out.println(i);
+		System.out.println(junk.get(0));
+    
+    
+    
+	}	
+
+}
